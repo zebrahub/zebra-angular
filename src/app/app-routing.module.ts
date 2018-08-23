3bbee09f0016f5c68f5a053a6037d9d6f934f33b
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2018-08-23 23:21:45 +0900

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [{ path: '/auth/github/callback', component: AppComponent }];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
